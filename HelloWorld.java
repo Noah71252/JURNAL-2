848e3482c08a658e1f9b88ca42d892935bd0cb14
Noah71252 <fayyazaastrid@gmail.com>
2024-09-19 15:56:53 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Astrid Fayyaza Danishara");
+        System.out.println("NIM: 607062400049");
+        System.out.println("Kelas:D3IF-48-01");
+    }
+}
